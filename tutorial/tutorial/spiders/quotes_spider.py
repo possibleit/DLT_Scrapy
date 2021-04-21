dcf2e363258111064a3d338d39710b1fb701dd7a
possibleit <possibleit@outlook.com>
2021-04-21 23:17:30 +0800

    修改了一些东西

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -7,11 +7,13 @@ from tutorial.utils import save2file, _save2file
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"  # yinli
+    allowed_domains = ["bzmfxz.com"]
 
     def start_requests(self):
         urls = [
             'http://www.bzmfxz.com/biaozhun/Soft/DLDLBZ/List_1.html',
         ]
+
         for url in urls:
             yield Request(url=url, callback=self.parse)
 
@@ -21,11 +23,11 @@ class QuotesSpider(scrapy.Spider):
         response是获取到的html文档
         //*[@id="main_right_box"]/div[2]/div[3]/div/div 选取到cmainbox属性的div
         '''
+        T = TutorialItem()
         time = response.xpath('//*[@id="main_right_box"]/div[2]/div[3]/div/div/div[@class="childclasslist_time"]/text()').extract()
         DLT = response.xpath('//*[@id="main_right_box"]/div[2]/div[3]/div/div/a[@target="_blank"]/text()').extract()
         link = response.xpath('//*[@id="pe100_page_infolist"]/a[last()-1]/@href')[0].extract()
 
-        T = TutorialItem()
         T['url_nxt'] = link
         for i, j in zip(time, DLT):
             T.item[j] = i
